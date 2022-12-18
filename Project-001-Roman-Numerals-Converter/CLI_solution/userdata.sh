b29abc4304bf03f38050dc82bf0c802566420f04
KeremKaragulle <kulelili3000@gmail.com>
2022-12-18 07:42:03 +0300

    AWS-PROJECTS

---
+++ Project-001-Roman-Numerals-Converter/CLI_solution/userdata.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+yum update -y
+yum install python3 -y
+pip3 install flask
+yum install git -y
+cd /home/ec2-user
+FOLDER="https://raw.githubusercontent.com/KeremKaragulle/my-projects/main/Project-001-Roman-Numerals-Converter"
+wget ${FOLDER}/app.py
+mkdir templates && cd templates
+wget ${FOLDER}/templates/index.html
+wget ${FOLDER}/templates/result.html
+cd ..
+python3 app.py
